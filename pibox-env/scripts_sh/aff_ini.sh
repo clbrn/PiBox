0ef434469b6eb043f8e3a189b248e470dab264a5
clbrn <51021179+clbrn@users.noreply.github.com>
2026-06-05 11:16:43 +0200

    Create aff_ini.sh

---
+++ pibox-env/scripts_sh/aff_ini.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides:          aff_inio
+# Required-Start:    $local_fs $remote_fs $network $syslog $named
+# Required-Stop:     $local_fs $remote_fs $network $syslog $named
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Affichage initial
+# Description:       Lancement su script aff_ini.py au démarrage
+### END INIT INFO
+
+cd /home/pibox/luma-env
+aff_ini.py
