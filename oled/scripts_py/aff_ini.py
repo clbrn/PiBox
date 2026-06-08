e40b33dec9d7402ad1309ac99345d0c17596e0fd
clbrn <51021179+clbrn@users.noreply.github.com>
2026-06-08 14:51:27 +0200

    Update aff_ini.py

--- oled/scripts_py/aff_ini.py
+++ oled/scripts_py/aff_ini.py
@@ -7,20 +7,20 @@ from PIL import ImageFont
 from time import sleep
 import serial
 import RPi.GPIO as GPIO
+import subprocess
 
 dvs20 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans",20)
 dvs10 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans",10)
 
 seri2c = i2c(port=1, address=0x3C)
-
 device = sh1106(seri2c)
 device.clear
 
 with canvas(device) as draw:
     draw.text((33, 4), "PiBox", font=dvs20, fill=255)
-    draw.text((0, 30), "En cours de démarrage", font=dvs10, fill=255)
-    draw.text((12, 45), "Veuillez parienter...", font=dvs10, fill=255)
-sleep(2)
+    draw.text((15, 30), "Démarrage en cours", font=dvs10, fill=255)
+    draw.text((24, 45), "Veuillez parienter...", font=dvs10, fill=255)
 
-
+sleep(5)
 device.persist = True
+subprocess.run(["home/pibox/luma_env/aff_oled.py"])
