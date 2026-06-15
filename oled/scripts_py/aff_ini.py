1a9446df6a414a77260f5d195cd168540ccddace
clbrn <51021179+clbrn@users.noreply.github.com>
2026-06-15 07:11:47 +0200

    Update aff_ini.py

--- oled/scripts_py/aff_ini.py
+++ oled/scripts_py/aff_ini.py
@@ -16,11 +16,27 @@ seri2c = i2c(port=1, address=0x3C)
 device = sh1106(seri2c)
 device.clear
 
+rep = subprocess.run(["runlevel"], capture_output=True, text=True)
+level = rep.stdout
+level =  level[0:3]
+print(level)
+if level == "N 3":
+   txt1 = "   Arret en cours "
+   txt2 = "    Au revoir..."
+else:
+   txt1 = "Démarrage en cours"
+   txt2 = "Veuillez parienter..."
+
 with canvas(device) as draw:
     draw.text((33, 4), "PiBox", font=dvs20, fill=255)
-    draw.text((15, 30), "Démarrage en cours", font=dvs10, fill=255)
-    draw.text((24, 45), "Veuillez parienter...", font=dvs10, fill=255)
+    draw.text((15, 30), txt1, font=dvs10, fill=255)
+    draw.text((24, 45), txt2, font=dvs10, fill=255)
 
 sleep(5)
 device.persist = True
-subprocess.run(["home/pibox/luma_env/aff_oled.py"])
+
+if level == "N 3":
+   device.persist = False
+   device.clear
+else:
+   subprocess.run(["/home/pibox/luma_env/aff_oled.py"])
